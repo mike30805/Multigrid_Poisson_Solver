773f4367028f1850d1a0747a8a68185cb17423cb
Chun-Yen Chen <chun-yenchen@Chun-YendeMacBook-Air.local>
2021-05-14 20:22:37 +0800

    Align

--- classes.h
+++ classes.h
@@ -1,10 +1,11 @@
 #ifndef CLASSES_H
 #define CLASSES_H
-#endif
-#include<stdlib.h>
-#include<iostream>
-#include<math.h>
+
+#include <stdlib.h>
+#include <iostream>
+#include <math.h>
 #include "vector"
+
 using namespace std;
 
 class matrix
@@ -12,13 +13,14 @@ class matrix
   int dim;
   double **value;
   double h;
-public:
+  
+  public:
     matrix(int n,double hi);
     ~matrix();
     void display();
     void Error(const matrix &b);
 
-    void SOR_smoothing(const matrix& rho,double omega,int steps);
+    void   SOR_smoothing(const matrix& rho,double omega,int steps);
     double averaging(int i,int j);
     matrix Restriction();
     double insertion(int i,int j,int dim_in);
@@ -30,7 +32,10 @@ public:
 
     double get_h();
     double get_dim();
-    void input_answer(int i,int j,double ans);
+    void   input_answer(int i,int j,double ans);
 
     matrix operator+(const matrix&);
-};
+}; // CLASS : matrix
+
+
+#endif // #ifndef CLASSES_H
