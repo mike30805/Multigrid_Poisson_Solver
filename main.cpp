c11be4aaa969b833ca9ad6a0ffa9f0592b4ffef9
ChunYen-Chen <chunyenchen2019@gmail.com>
2021-05-14 22:44:45 +0800

    Add W_cycle function.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #define PI 3.14159265359
 
 
-double f(double x,double y)
+double f( double x,double y )
 {
   return sin(x)*sin(y);
 } // double f(double x,double y)
 
 
 
-void solved(matrix &m)
+void solved( matrix &m )
 {
   int dim = m.get_dim();
   for(int i=0; i<dim; i++)
@@ -27,39 +27,44 @@ void solved(matrix &m)
 
 
 
-matrix V_Cycle(matrix phi,matrix dens)
+matrix V_Cycle(matrix phi, matrix dens)
 {
   // Pre-Smoothing
-  phi.SOR_smoothing(dens,1.7,3);
+  phi.SOR_smoothing( dens, 1.7, 3 );
 
   // Compute Residual Errors
-  matrix r = phi.Residual(dens);
+  matrix r = phi.Residual( dens );
 
   // Restriction
   matrix rhs = r.Restriction();
   
-  matrix eps(rhs.get_dim(),rhs.get_h());
+  matrix eps( rhs.get_dim(), rhs.get_h() );
   
   // stop recursion at smallest grid size, otherwise continue recursion
   if ( rhs.get_dim() <= 5 )
   {
     matrix dens2 = dens.Restriction();
-    eps.SOR_smoothing(dens2,1.7,100);
+    eps.SOR_smoothing( dens2, 1.7, 100 );
   } // if ( rhs.get_dim() <= 5 )
   else
   {
-    eps = V_Cycle(eps,rhs);  
+    eps = V_Cycle( eps, rhs );  
   } // if ( rhs.get_dim() <= 5 ) ... else ...
 
   // Prolongation and Correction
-  phi = phi + eps.Interpolation(phi.get_dim());
+  phi = phi + eps.Interpolation( phi.get_dim() );
 	
   // Post-Smoothing
-  phi.SOR_smoothing(dens,1.7,3);
+  phi.SOR_smoothing( dens, 1.7, 3 );
   
   return phi;
 } // FUNCTION : V_Cycle
 
+
+matrix W_Cycle( matrix phi, matrix dens )
+{
+
+} // FUNCTION : W_Cycle
 
 
 int main()
