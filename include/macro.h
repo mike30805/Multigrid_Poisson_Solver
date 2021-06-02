4c4b4a019cb0f452c595cba08d7eb3e89df6c993
ChunYen-Chen <chunyenchen2019@gmail.com>
2021-06-03 02:14:34 +0800

    Add particle mass deposition option. Fix BOX_DX.

--- include/macro.h
+++ include/macro.h
@@ -2,9 +2,19 @@
 #define PI              3.14159265359
 
 
+// box
+#define BOX_DX          BOX_L / BOX_N       // cell center grid
+
+
 // solver type
 #define SOR             1
 #define V_CYCLE         2
 #define W_CYCLE         3
 #define FAS             4
 #define FMG             5
+
+
+// particle mass to cell mass
+#define NGP             1                       // Nearest-Grid-Point
+#define CIC             2                       // Cloud-in-Cell
+#define TSC             3                       // Triangular-Shape-Cloud
